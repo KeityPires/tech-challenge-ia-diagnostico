6eb3735eb659fb9c3d9cdb77eef96a0b8445d862
KeityPires <keityrcpires@gmail.com>
2026-05-14 23:58:52 -0300

    Atualização media_utils

--- src/multimodal/media_utils.py
+++ src/multimodal/media_utils.py
@@ -1,15 +1,12 @@
 from pathlib import Path
-from moviepy.editor import VideoFileClip
+from moviepy import VideoFileClip
 
 
-def extract_audio_from_video(video_path: str, output_audio_path: str | None = None, audio_format: str = "wav") -> str:
-    """
-    Extrai o áudio de um vídeo e salva em WAV ou MP3.
-
-    Retorna:
-        Caminho do áudio gerado.
-    """
-
+def extract_audio_from_video(
+    video_path: str,
+    output_audio_path: str | None = None,
+    audio_format: str = "wav"
+) -> str:
     video_file = Path(video_path)
 
     if not video_file.exists():
@@ -23,7 +20,6 @@ def extract_audio_from_video(video_path: str, output_audio_path: str | None = No
         )
 
     output_audio_file = Path(output_audio_path)
-
     output_audio_file.parent.mkdir(
         parents=True,
         exist_ok=True
@@ -33,14 +29,12 @@ def extract_audio_from_video(video_path: str, output_audio_path: str | None = No
 
     if video.audio is None:
         video.close()
-
         raise ValueError(
             "O vídeo não possui faixa de áudio."
         )
 
     video.audio.write_audiofile(
         str(output_audio_file),
-        verbose=False,
         logger=None
     )
 
